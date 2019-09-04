2ed2d6dcb30e286ff941114ea8a2f6c46960d78f
emy-sun <45594002+emy-sun@users.noreply.github.com>
2019-09-04 11:36:01 +0300

    finished project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
+  moveFirstElement = false;
+  moveSecondElement = false;
+  zIndex = 2;
+    
+  onTransitionFirst() {
+    this.moveFirstElement = true;
+
+    setTimeout(() => {
+      this.zIndex = this.zIndex === 2 ? 0 : 2;
+      this.moveFirstElement = false;
+    }, 500);
+  }
+
+  onTransitionSecond() {
+    this.moveSecondElement = true;
+    
+    setTimeout(() => {
+      this.zIndex = this.zIndex === 2 ? 0 : 2;
+      this.moveSecondElement = false;
+    }, 500);
+  }
 }
